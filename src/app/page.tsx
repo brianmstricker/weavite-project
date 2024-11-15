5015fd1289e902dcc6726235f7688a4b83af3d32
brian <brianmstricker@gmail.com>
2024-11-14 23:54:12 -0500

    style: tweaked image animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default async function Home() {
  return (
   <div className="grid grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 min-[1400px]:grid-cols-8 gap-4">
    {allData.map((item) => (
-    <Card key={item.item_ID} className="overflow-hidden">
-     <div className="relative cursor-pointer overflow-hidden max-w-full w-full h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px]">
+    <Card key={item.item_ID} className="overflow-hidden transition-all duration-200 ease-in-out hover:rounded-t-none border-2">
+     <div className="relative cursor-pointer overflow-hidden aspect-square">
       <Image
        src={`data:image/jpeg;base64,${item.image}`}
        alt={item.title}
        fill
-       className="transition-all duration-200 hover:scale-[1.003] contrast-[1.05] hover:contrast-[1.08] ease-in-out"
+       className="transition-all duration-200 ease-in-out contrast-[1.03] hover:contrast-[1.05]"
       />
      </div>
      <div className="p-2">
